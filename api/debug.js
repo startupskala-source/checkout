d8385eb09558e2b9a70bfc165f567a57b6aab45d
Sidinei <sidinho1kaa@gmail.com>
2026-07-03 13:36:21 -0300

    fix: debug

--- api/debug.js
+++ api/debug.js
@@ -3,6 +3,7 @@ module.exports = async (req, res) => {
   res.json({
     has_token: !!process.env.MP_ACCESS_TOKEN,
     has_key: !!process.env.MP_PUBLIC_KEY,
+    key_value: process.env.MP_PUBLIC_KEY ? process.env.MP_PUBLIC_KEY.substring(0, 15) : 'none',
     token_prefix: process.env.MP_ACCESS_TOKEN ? process.env.MP_ACCESS_TOKEN.substring(0, 10) : 'none',
     node_version: process.version
   });
